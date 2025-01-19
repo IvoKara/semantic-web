0b810802146a4f0bdd93b156ee096a22228a229c
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2025-01-19 21:12:17 +0200

    feat: use dynamic query string and ontolugy file to get the result from SPARQL query

--- src/semantic_web/App.java
+++ src/semantic_web/App.java
@@ -1,20 +1,69 @@
 package semantic_web;
 
+import java.util.Arrays;
+
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.vocabulary.VCARD;
+import org.apache.jena.riot.RDFDataMgr;
 
 public class App {
 
-    static String personalURI = "http://somewhere/JohnSmith";
-    static String fullName     = "John Smith";
+    static String ontologyFile = "project-management.owl";
+    static String theQuery = OntologyQueries.WHO_FUNDS_A_GIVEN_PROJECT;
 
     public static void main(String[] args) throws Exception {
-        Model model = ModelFactory.createDefaultModel();
 
-        Resource johnSmith = model.createResource(personalURI);
+        Model model = RDFDataMgr.loadModel(ontologyFile);
+        Dataset dataset = DatasetFactory.create(model);
+        try {
+            executeQuery(theQuery, dataset);
+        } finally {
+            model.close();
+            dataset.close();
+        }
+    }
 
-        johnSmith.addProperty(VCARD.FN, fullName);
+    public static void executeQuery(String queryString, Dataset dataset) {
+        String[] variables = getSelectVariables(queryString);
+
+        Query query = QueryFactory.create(queryString);
+        QueryExecution queryExecution = QueryExecutionFactory.create(query, dataset);
+
+        try {
+            ResultSet results = queryExecution.execSelect();
+            while (results.hasNext()) {
+                QuerySolution solution = results.nextSolution();
+
+                System.out.println("------------");
+                for (String variable : variables) {
+                    System.out.println(variable + ": " + solution.get(variable));
+                }
+            }
+        } finally {
+            queryExecution.close();
+            dataset.close();
+            System.out.println("------------");
+        }
+    }
+
+    public static String[] getSelectVariables(String queryString) {
+        String[] lines = queryString.split("\n");
+
+        for (String line : lines) {
+            if (line.trim().startsWith("SELECT")) {
+                String[] lineItems = line.split(" \\?");
+                String[] queryVariables = Arrays.copyOfRange(lineItems, 1, lineItems.length);
+                return queryVariables;
+            }
+        }
+
+        return new String[] {};
     }
 }
